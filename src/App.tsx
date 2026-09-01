56b7922ec87a3a1426a7f3a21a81749970950051
Mateus Souza <mateussouzaprogrammer@gmail.com>
2026-09-01 17:02:10 -0400

    sideBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import './App.css'
-import Sidebar from "./components/Sidebar/Sidebar"
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
 
+  const [sidebarAberta, setSidebarAberta] = useState(false);
+
   return (
     <div>
-      <Sidebar />
+
+      {/* Botão */}
+      <button
+        onClick={() => setSidebarAberta(!sidebarAberta)}
+        className="fixed top-4 left-4 z-50 p-2 rounded-lg bg-blue-500 text-white"
+      >
+        {sidebarAberta ? <X size={24} /> : <Menu size={24} />}
+      </button>
+
+      {/* Sidebar */}
+      {sidebarAberta && <Sidebar />}
+
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
